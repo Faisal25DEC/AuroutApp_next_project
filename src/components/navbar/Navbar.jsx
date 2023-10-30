48adc76a7abd004c53efcaf01f002329635a2309
Faisal25DEC <faisalmd25121999@gmail.com>
2023-10-31 03:10:42 +0530

    updated frontend

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -45,7 +45,7 @@ const Navbar = () => {
   return (
     <div className={styles.container}>
       <Link href="/" className={styles.logo}>
-        lamamia
+        Faisal
       </Link>
       <div className={styles.links}>
         <DarkModeToggle />
